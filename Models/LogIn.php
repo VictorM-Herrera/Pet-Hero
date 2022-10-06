3218a7bebb03f5a45fab4517d9bff21ce9bde800
lc2101 <lrcroci@gmail.com>
2022-10-06 20:32:52 -0300

    form ingreso

---
+++ Models/LogIn.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LogIn</title>
+</head>
+<body>
+
+<input type="email" name="Email" id="">
+<br>
+
+<input type="password" name="password" id="">
+<br>
+<button type="submit">Ingresar</button>
+    
+</body>
+</html>
