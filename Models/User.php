8551438ba4f56a9531099b606858dc01255de6e7
VictorM-Herrera <herreramaxi@yahoo.com>
2022-10-06 21:05:09 -0300

    Actualizacion user

--- Models/User.php
+++ Models/User.php
@@ -1,15 +1,13 @@
 <?php
 namespace Models;
 abstract class User{
-    private $userId;
+    private $name;
+    private $lastName;
+    private $birthDay;
     private $email;
     private $password;
 
-    public function getUserId()
-    {
-        return $this->userId;
-    }
-
+    
     /**
      * Set the value of userId
      *
@@ -58,6 +56,66 @@ abstract class User{
     public function setPassword($password)
     {
         $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of lastName
+     */ 
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * Set the value of lastName
+     *
+     * @return  self
+     */ 
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of birthDay
+     */ 
+    public function getBirthDay()
+    {
+        return $this->birthDay;
+    }
+
+    /**
+     * Set the value of birthDay
+     *
+     * @return  self
+     */ 
+    public function setBirthDay($birthDay)
+    {
+        $this->birthDay = $birthDay;
 
         return $this;
     }
