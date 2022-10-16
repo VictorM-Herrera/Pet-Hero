c06b3b59718f3a1d5a828be64312b6bd8307c881
lc2101 <lrcroci@gmail.com>
2022-10-16 19:29:23 -0300

    guia mains
    form pets
    bases mains watcher y owner

---
+++ Visual/MainOwner.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dueño</title>
+</head>
+<body>
+    <a href="">Guardianes</a>
+    <a href="">Mascotas</a>
+    <a href="">Reservas</a>
+    
+</body>
+</html>
