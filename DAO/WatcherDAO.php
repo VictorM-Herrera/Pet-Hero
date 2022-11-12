5a76a2e320b2efe277188a5dd4957599904c75ad
lc2101 <lrcroci@gmail.com>
2022-11-12 20:46:36 -0300

    12.11 20:46

--- DAO/WatcherDAO.php
+++ DAO/WatcherDAO.php
@@ -1,22 +1,16 @@
 <?php
     namespace DAO;
 
-    use DAO\ReviewDAO as ReviewDAO;
-    use Models\Guardian as Guardian;
-    use Models\Review as Review;
-    // use to bdd
+
+    use Models\Watcher as Watcher;
     use DAO\Connection as Connection;
-    use Exception;
-    use PDOException;
+    use \Exception as Exception;
 
     class WatcherDAO
     {
-        private $guardianList = array();
-        private $fileName = ROOT . "Data/Guardianes.json";
-
         private $connection;
 
-        public function Add(Guardian $guardian)
+        public function Add(Watcher $watcher)
         {
             try
             {
@@ -99,154 +93,6 @@
                 return null;
             }
         }
-
-        public function GetByDni($dni){
-
-            $guardList = array();
-
-            try {
-                $this->connection = Connection::GetInstance();
-                $query = "SELECT * FROM guardians WHERE dni = '$dni' ";
-                $rta = $this->connection->Execute($query);
-            } 
-            catch (Exception $e) 
-            {
-                throw $e;
-            }
-
-            if(!empty($rta))
-            {
-                foreach ($rta as $row) 
-                {
-                    $guardian = $this->map($row);
-                    array_push($guardList, $guardian);
-                }
-
-                return $guardList[0];  
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        public function GetByEmail($email){
-            
-            $guardList = array();
-
-            try {
-                $this->connection = Connection::GetInstance();
-                $query = "SELECT * FROM guardians WHERE email = '$email' ";
-                $rta = $this->connection->Execute($query);
-            } 
-            catch (Exception $e) 
-            {
-                throw $e;
-            }
-
-            if(!empty($rta))
-            {
-                foreach ($rta as $row) 
-                {
-                    $guardian = $this->map($row);
-                    array_push($guardList, $guardian);
-                }
-
-                return $guardList[0];  
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        public function GetIdSize($size)
-        {
-            try 
-            {
-                $this->connection = Connection::GetInstance();
-                $query = "SELECT id_size FROM sizes WHERE size = '$size' ";
-                $rta = $this->connection->Execute($query);
-            } 
-            catch (Exception $e) 
-            {
-                throw $e;
-            }
-
-            if (!empty($rta))
-            {
-                return $rta[0][0];
-            }
-        }
-
-        public function GetSize($id)
-        {
-            try 
-            {
-                $this->connection = Connection::GetInstance();
-                $query = "SELECT size FROM sizes WHERE id_size = '$id' ";
-                $rta = $this->connection->Execute($query);
-            } 
-            catch (Exception $e) 
-            {
-                throw $e;
-            }
-
-            if (!empty($rta))
-            {
-                return $rta[0][0];
-            }
-        }
-
-        public function Update($id, Guardian $guardian)
-        {
-            try
-            {
-                $this->connection = Connection::GetInstance();
-
-                $query = "UPDATE guardians SET first_name=:first_name, last_name=:last_name, telephone=:telephone, address=:address, pass=:pass, id_size_care=:id_size_care, cost=:cost
-                            WHERE id_guardian = '$id'";
-
-                $parameters['first_name'] = $guardian->getName();
-                $parameters['last_name'] = $guardian->getLast_name();
-                $parameters['telephone'] = $guardian->getTelephone();
-                $parameters['address'] = $guardian->getAddress();
-                $parameters['pass'] = $guardian->getPassword();
-
-                if (($guardian->getSizeCare() != 1) || ($guardian->getSizeCare() != 2) || ($guardian->getSizeCare() != 3))
-                {
-                    $parameters['id_size_care'] = $this->GetIdSize($guardian->getSizeCare());
-                }
-                else
-                {
-                    $parameters['id_size_care'] = $guardian->getSizeCare();
-                }
-
-                $parameters['cost'] = $guardian->getCost();
-
-                $this->connection->ExecuteNonQuery($query, $parameters);
-            }
-            catch (Exception $e)
-            {
-                throw $e;
-            }
-        }
-
-        public function Remove($id)
-        {
-            try {
-                $this->connection = Connection::GetInstance();
-                $query = "DELETE FROM guardians WHERE id_guardian = '$id' ";
-                $rta = $this->connection->ExecuteNonQuery($query);
-                
-                return $rta;
-            } 
-            catch (Exception $e) 
-            {
-                throw $e;
-            }
-        }
-
         protected function map ($rta)
         {
             $guardian = new Guardian;
